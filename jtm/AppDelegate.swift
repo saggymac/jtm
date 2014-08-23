5e61153a640faf44974de1d9c91faee39b546c49
sguyer <saguyer@gmail.com>
2014-08-23 00:01:13 -0400

    Initial Commit

---
+++ jtm/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  jtm
+//
+//  Created by Scott A. Guyer on 8/23/14.
+//  Copyright (c) 2014 Scott A. Guyer. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ jtm/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  jtm
+//
+//  Created by Scott A. Guyer on 8/23/14.
+//  Copyright (c) 2014 Scott A. Guyer. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
